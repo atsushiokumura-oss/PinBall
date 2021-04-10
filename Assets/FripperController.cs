58668870eb141edbc6bc41ceb5f0888594ac8ac1
atsushiokumura-oss <81137612+atsushiokumura-oss@users.noreply.github.com>
2021-04-10 17:46:37 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+
+    private float flickAngle = -20;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defaultAngle);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle (this.flickAngle);
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripper")
+        {
+            SetAngle (this.defaultAngle);
+
+        }
+
+    }
+
+
+}
+
+public void SetAngle(float angle)
+{
+    jointSpring jointSpr = this.myHingeJoint.spring;
+    jointSpr.targetPostion = angle;
+    this.myHingeJoint.spring = jointSpr;
+
+}
